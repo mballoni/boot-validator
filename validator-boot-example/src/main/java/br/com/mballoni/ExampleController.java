480bf4df5387072d828ea0d5799e50b530be509a
MBalloni <marceloballoni@gmail.com>
2019-06-25 22:04:13 -0300

    [0] Adds missing request body identification

--- validator-boot-example/src/main/java/br/com/mballoni/ExampleController.java
+++ validator-boot-example/src/main/java/br/com/mballoni/ExampleController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
 
     @PostMapping(value ="/doit",consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void doItNow(@Validated ExampleRequest request) {
+    public void doItNow(@RequestBody @Validated ExampleRequest request) {
         log.info("Request Object: {}", request);
     }
 }
