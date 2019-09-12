c993ac6c4b0b43d571f1082b4a3d28cff83ef018
MBalloni <marceloballoni@gmail.com>
2019-09-11 23:10:49 -0300

    #0 Starter testing default beans

---
+++ validator-boot-core/src/test/java/br/com/mballoni/autoconfigure/ValidationAutoConfigurationTest.java
@@ -0,0 +1,40 @@
+package br.com.mballoni.autoconfigure;
+
+
+import br.com.mballoni.validatorboot.BeanValidation;
+import br.com.mballoni.validatorboot.ValidationErrorAdvice;
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidationAutoConfigurationTest {
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(ValidationAutoConfiguration.class));
+
+    @Test
+    public void setup_DEFAULT_beans() {
+        contextRunner.withUserConfiguration(DefaultConfiguration.class)
+                .run((context) -> {
+                    assertThat(context).hasSingleBean(BeanValidation.class);
+                    assertThat(context).hasSingleBean(ValidationErrorAdvice.class);
+                });
+    }
+
+    @Configuration
+    static class DefaultConfiguration {
+
+        @Bean
+        public Validator validator() {
+            return Validation.buildDefaultValidatorFactory().getValidator();
+        }
+    }
+
+}
