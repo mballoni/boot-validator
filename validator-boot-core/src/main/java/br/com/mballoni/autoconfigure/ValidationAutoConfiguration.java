926ee975941c90141c5a762d7f5f44ad501d15a4
mballoni <marceloballoni@gmail.com>
2020-06-06 10:09:38 -0300

    Bumps Spring Boot to 2.3

--- validator-boot-core/src/main/java/br/com/mballoni/autoconfigure/ValidationAutoConfiguration.java
+++ validator-boot-core/src/main/java/br/com/mballoni/autoconfigure/ValidationAutoConfiguration.java
@@ -3,9 +3,11 @@ package br.com.mballoni.autoconfigure;
 import br.com.mballoni.validatorboot.BeanValidation;
 import br.com.mballoni.validatorboot.ValidationErrorAdvice;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 import javax.validation.Validator;
 
+@Import(org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration.class)
 public class ValidationAutoConfiguration {
 
     @Bean
